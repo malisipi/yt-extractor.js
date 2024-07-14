f8bb955d727ff90152c630fecc5af49e74a14e79
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2024-07-15 00:42:56 +0300

    Support owner data extraction

--- src/owner.js
+++ src/owner.js
@@ -7,7 +7,16 @@ const parser = new xml_parser.XMLParser({
 var owner = {
     get_owner: async channel_id => {
         let page = await utils.get_text(`https://www.youtube.com/channel/${encodeURIComponent(channel_id)}`);
-        return utils.extract_json_data_from_page(page, "ytInitialData");
+        let data = utils.extract_json_data_from_page(page, "ytInitialData");
+        return ({
+            name: data?.metadata?.channelMetadataRenderer?.title,
+            id: data?.microformat?.microformatDataRenderer?.urlCanonical?.split("/channel/")?.[1] ?? channel_id,
+            description: data?.metadata?.channelMetadataRenderer?.description,
+            thumbnails: data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.image?.decoratedAvatarViewModel?.avatar?.avatarViewModel?.image?.sources ?? data?.metadata?.channelMetadataRenderer?.avatar?.thumbnails ?? [],
+            videosCount: Number(JSON.stringify(data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.metadata?.contentMetadataViewModel?.metadataRows)?.match(/[0-9\.]+ video/g)?.[0]?.match(/[0-9]+/g)?.[0] ?? 0),
+            videos: await owner.get_owner_videos(channel_id),
+            followers: JSON.stringify(data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.metadata?.contentMetadataViewModel?.metadataRows)?.match(/[0-9\.KM]+ subscribers/g)?.[0]?.split(" ")?.[0]?.replace("K"," 1000")?.replace("M", " 1000000")?.split(" ")?.reduce((total, current) => {return total*Number(current)},1)
+        });
     },
     get_owner_videos: async channel_id => {
         let rss = await (await fetch(`https://www.youtube.com/feeds/videos.xml?channel_id=${encodeURIComponent(channel_id)}`)).text()
