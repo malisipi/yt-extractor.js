db58193ee76fc0334cffc1981c809c0c272def9a
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2024-07-15 01:20:49 +0300

    Fix videos part of owner data

--- src/owner.js
+++ src/owner.js
@@ -15,7 +15,7 @@ var owner = {
             backgrounds: data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.banner?.imageBannerViewModel?.image?.sources ?? [],
             thumbnails: data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.image?.decoratedAvatarViewModel?.avatar?.avatarViewModel?.image?.sources ?? data?.metadata?.channelMetadataRenderer?.avatar?.thumbnails ?? [],
             videosCount: Number(JSON.stringify(data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.metadata?.contentMetadataViewModel?.metadataRows)?.match(/[0-9\.]+ video/g)?.[0]?.match(/[0-9]+/g)?.[0] ?? 0),
-            videos: await owner.get_owner_videos(channel_id),
+            videos: (await owner.get_owner_videos(channel_id)).entry,
             followers: JSON.stringify(data?.header?.pageHeaderRenderer?.content?.pageHeaderViewModel?.metadata?.contentMetadataViewModel?.metadataRows)?.match(/[0-9\.KM]+ subscribers/g)?.[0]?.split(" ")?.[0]?.replace("K"," 1000")?.replace("M", " 1000000")?.split(" ")?.reduce((total, current) => {return total*Number(current)},1)
         });
     },
