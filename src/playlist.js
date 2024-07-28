502f21a8c131f6beb4c8b359540cd80bf9c4730f
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2024-07-28 22:10:50 +0300

    Support fallback values for playlists

--- src/playlist.js
+++ src/playlist.js
@@ -4,19 +4,19 @@ var playlist = {
     get_playlist: async (playlist_id) => {
         let page = await utils.get_text(`https://youtube.com/playlist?list=${playlist_id}`);
         let data = utils.extract_json_data_from_page(page, "ytInitialData");
-        let playlist = data.contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents.map(a=>a.playlistVideoRenderer).filter(a=>a!=undefined);
+        let playlist = data?.contents?.twoColumnBrowseResultsRenderer?.tabs?.[0]?.tabRenderer?.content?.sectionListRenderer?.contents?.[0]?.itemSectionRenderer?.contents?.[0]?.playlistVideoListRenderer?.contents?.map(a=>a?.playlistVideoRenderer)?.filter(a=>a!=undefined) ?? null;
 
-        return playlist.map(video => ({
-            id: video.videoId,
-            title: video.title.runs[0].text,
-            thumbnails: video.thumbnail.thumbnails,
-            views: Number(video.title.accessibility.accessibilityData.label.match(/[0-9\.\,\ ]+view/g)[0].replace(/[A-Za-z\ \.\,]+/g, "")),
-            length: Number(video.lengthSeconds),
+        return playlist?.map(video => ({
+            id: video?.videoId ?? null,
+            title: video?.title?.runs?.[0]?.text ?? "",
+            thumbnails: video?.thumbnail?.thumbnails ?? [],
+            views: Number(video?.title?.accessibility?.accessibilityData?.label?.match(/[0-9\.\,\ ]+view/g)[0].replace(/[A-Za-z\ \.\,]+/g, "") ?? 0),
+            length: Number(video?.lengthSeconds ?? 0),
             owner: {
-                name: video.shortBylineText.runs[0].text,
-                id: video.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url.replace("/channel/","")
+                name: video?.shortBylineText?.runs?.[0]?.text ?? "",
+                id: video?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url?.replace("/channel/","") ?? null
             },
-        }))
+        })) ?? null;
     }
 };
 
