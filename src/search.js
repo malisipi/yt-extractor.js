673fea579545926325a87ed81e14ea7e7c63ddee
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2025-04-26 03:16:23 +0300

    Update view extraction for also search

--- src/search.js
+++ src/search.js
@@ -9,15 +9,15 @@ var search = {
         let data = await search.search_data(query);
         let video_datas = data.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents;
         return video_datas.map(a=>a.videoRenderer).filter(a=>a!=undefined).map(video => ({
-            id: video.videoId,
-            title: video.title.runs[0].text,
-            thumbnails: video.thumbnail.thumbnails,
-            views: Number(video.title.accessibility.accessibilityData.label.match(/[0-9\.\,\ ]+view/g)[0].replace(/[A-Za-z\ \.\,]+/g, "")),
+            id: video?.videoId,
+            title: video?.title?.runs?.[0]?.text,
+            thumbnails: video?.thumbnail?.thumbnails,
+            views: Number(video?.shortViewCountText?.accessibility?.accessibilityData?.label?.replace(" million", "M")?.match(/[0-9a-zA-Z\.]+/g)?.[0]?.replace("K"," 1000")?.replace("M", " 1000000")?.split(" ")?.reduce((total, current) => {return total*Number(current)},1) ?? 0),
             length: utils.extract_time_from_text(video?.lengthText?.simpleText) ?? null,
             owner: {
-                name: video.shortBylineText.runs[0].text,
-                verified: (video.ownerBadges?.filter(a=>a.metadataBadgeRenderer?.style?.includes("VERIFIED")).length ?? 0) > 0,
-                id: video.shortBylineText.runs[0].navigationEndpoint.commandMetadata.webCommandMetadata.url.replace("/channel/","")
+                name: video?.shortBylineText?.runs?.[0]?.text,
+                verified: (video?.ownerBadges?.filter(a=>a.metadataBadgeRenderer?.style?.includes("VERIFIED")).length ?? 0) > 0,
+                id: video?.shortBylineText?.runs?.[0]?.navigationEndpoint?.commandMetadata?.webCommandMetadata?.url?.replace("/channel/","")
             },
         }));
     },
